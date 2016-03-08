6c88b3ebb322212db375c54371225bfbfe3e81aa
Giovanni  Bruno <giovanni.bruno@soluzionifutura.it>
2016-03-08 11:32:43 +0100

    introdotto browser sync da fixare e watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-var gulp    = require('gulp');
-var jade    = require('gulp-jade');
-var uglify  = require('gulp-uglify');
-var cssnano = require('gulp-cssnano');
-var del     = require('del');
+var gulp        = require('gulp');
+var jade        = require('gulp-jade');
+var uglify      = require('gulp-uglify');
+var cssnano     = require('gulp-cssnano');
+var del         = require('del');
+var browserSync = require('browser-sync');
 
 gulp.task('clean', function(){
    return del.sync('./dist');
@@ -32,6 +33,25 @@ gulp.task('fonts', function(){
         .pipe(gulp.dest('./dist/fonts/'));
 });
 
+gulp.task('watch', function(){
+  gulp.watch('./src/views/**/*',  ['jade']);
+  gulp.watch('./src/scripts/**/*',  ['js']);
+  gulp.watch('./src/styles/**/*',  ['css']);
+  gulp.watch('./src/fonts/**/*',  ['fonts']);
+})
+
+gulp.task('browser-sync', function () {
+    browserSync({
+      // informs browser-sync to proxy our expressjs app which would run at the following location
+      // informs browser-sync to use the following port for the proxied app
+      // notice that the default port is 3000, which would clash with our expressjs
+      server: './dist',
+      baseDir: './',
+      port: 4000,
+      notify: false
+  });
+});
+
 gulp.task('build', ['clean', 'jade', 'css', 'js', 'fonts']);
 
-gulp.task('default', ['build']);
+gulp.task('default', ['build', 'browser-sync', 'watch']);
