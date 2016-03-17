e54427bd3e87ccd8624d0126a8f98d340b6d16a2
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2016-03-17 23:47:50 +0100

    fix importanti, aggiunto concatJs, concatCss, imageMin

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
+var pngquant    = require('imagemin-pngquant');
+var del         = require('del');
+var browserSync = require('browser-sync');
 var gulp        = require('gulp');
+var imagemin    = require('gulp-imagemin');
 var jade        = require('gulp-jade');
 var uglify      = require('gulp-uglify');
 var cssnano     = require('gulp-cssnano');
-var del         = require('del');
-var browserSync = require('browser-sync');
+var concatJs    = require('gulp-concat');
+var concatCss   = require('gulp-concat-css');
 
 gulp.task('clean', function(){
    return del.sync('./dist');
@@ -11,42 +15,53 @@ gulp.task('clean', function(){
 
 gulp.task('jade', function(){
 	gulp.src('./src/views/pages/*.jade')
-    	.pipe(jade())
-    	.pipe(gulp.dest('./dist/'));
+  	.pipe(jade())
+  	.pipe(gulp.dest('./dist/'));
 });
 
-
 gulp.task('css', function(){
-    gulp.src('./src/styles/**/*')
-        .pipe(cssnano())
-        .pipe(gulp.dest('./dist/styles/'));
+  gulp.src(['./src/styles/vendor/*.css', './src/styles/*.css'])
+    .pipe(cssnano())
+    .pipe(concatCss('styles.css'))
+    .pipe(gulp.dest('./dist/styles/'));
 });
 
 gulp.task('js', function(){
-    gulp.src('./src/scripts/**/*')
-        .pipe(uglify())
-        .pipe(gulp.dest('./dist/scripts/'));
+  gulp.src(['./src/scripts/vendor/*.js', './src/scripts/*.js'])
+    .pipe(uglify())
+    .pipe(concatJs('scripts.js'))
+    .pipe(gulp.dest('./dist/scripts/'));
+});
+
+gulp.task('imagemin', function(){
+  gulp.src('src/images/*')
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('fonts', function(){
-    gulp.src('./src/fonts/**/*')
-        .pipe(gulp.dest('./dist/fonts/'));
+  gulp.src('./src/fonts/**/*')
+    .pipe(gulp.dest('./dist/fonts/'));
 });
 
 gulp.task('watch', function(){
   browserSync({
-    server: './dist',
-    baseDir: './',
-    port: 4000,
-    notify: false
+  server: './dist',
+  baseDir: '/',
+  port: 4000,
+  notify: false
   });
 
-  gulp.watch('./src/views/**/*',  ['jade']).on('change', browserSync.reload);
-  gulp.watch('./src/scripts/**/*',  ['js']).on('change', browserSync.reload);
-  gulp.watch('./src/styles/**/*',  ['css']).on('change', browserSync.reload);
-  gulp.watch('./src/fonts/**/*',  ['fonts']).on('change', browserSync.reload);
+  gulp.watch('./src/views/**/*', ['jade']).on('change', browserSync.reload);
+  gulp.watch('./src/scripts/**/*', ['js']).on('change', browserSync.reload);
+  gulp.watch('./src/styles/**/*', ['css']).on('change', browserSync.reload);
+  gulp.watch('./src/fonts/**/*', ['fonts']).on('change', browserSync.reload);
 });
 
-gulp.task('build', ['clean', 'jade', 'css', 'js', 'fonts']);
+gulp.task('build', ['clean', 'imagemin', 'jade', 'css', 'js', 'fonts']);
 
 gulp.task('default', ['build', 'watch']);
